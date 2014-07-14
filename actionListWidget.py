6ea3199dada44db6d728d1e886af8c38f94eb5de
Matt Gibbs <mgibbs@dhcpvisitor21980.slac.stanford.edu>
2014-07-14 01:03:12 -0700

    Tiny little whitespace tweak.

--- actionListWidget.py
+++ actionListWidget.py
@@ -7,7 +7,7 @@ class ActionListWidget(QtGui.QListWidget):
         QtGui.QListWidget.__init__(self, *args)
     
     def keyPressEvent(self, key_event):
-        if ((key_event.key()==QtCore.Qt.Key_Return) or (key_event.key()==QtCore.Qt.Key_Enter)):
+        if ((key_event.key() == QtCore.Qt.Key_Return) or (key_event.key() == QtCore.Qt.Key_Enter)):
             self.returnKeyPressed.emit()
         else:
             return QtGui.QListWidget.keyPressEvent(self, key_event)
