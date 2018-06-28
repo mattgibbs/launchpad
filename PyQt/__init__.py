ad0de8e576d5ec281d3719e8cb235344d9ed7481
Matt Gibbs <mgibbs@slac.stanford.edu>
2018-06-28 10:39:02 -0700

    Add PyQt4/5 compatibility layer.

---
+++ PyQt/__init__.py
@@ -0,0 +1,5 @@
+"""
+Thin wrapper to give us a unified import interface for Qt4 and Qt5.
+
+Extracted from PyDM.
+"""
---
+++ PyQt/qtlib.py
@@ -0,0 +1,18 @@
+QT_LIB = None
+lib_order = ['PyQt5', 'PyQt4']
+for lib in lib_order:
+    if lib in sys.modules:
+        QT_LIB = lib
+        break
+
+if QT_LIB is None:
+    for lib in lib_order:
+        try:
+            __import__(lib)
+            QT_LIB = lib
+            break
+        except ImportError:
+            pass
+
+if QT_LIB is None:
+    raise ImportError("PyQt4 or PyQt5 could not be imported.")
