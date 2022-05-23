e2e26f391a130e43437f5caf0671ec7fb5557ae3
zimmerc <zimmerc@slac.stanford.edu>
2022-05-23 16:13:32 -0700

    PyQt5 explicit, direct .ui file import

--- actionDetailDialog.py
+++ actionDetailDialog.py
@@ -1,18 +1,19 @@
-from PyQt import QtCore, QtGui
-from actionDetailDialog_ui import Ui_actionDetailDialog
+from PyQt5 import QtWidgets, uic
+import os
 
-class ActionDetailDialog(QtGui.QDialog):
+class ActionDetailDialog(QtWidgets.QDialog):
     Deleted = 666 #custom result code signaling a request to delete the action.
     def __init__(self, parent=None):
-        QtGui.QDialog.__init__(self, parent)
-        self.ui = Ui_actionDetailDialog()
-        self.ui.setupUi(self)
+        QtWidgets.QDialog.__init__(self, parent)
+        self.ui = uic.loadUi(os.path.join(os.path.dirname(os.path.abspath(__file__)), "actionDetailDialog.ui"), self)
         self.ui.deleteButton.clicked.connect(self.return_delete_result_code)
         
     def show_with_action(self,action):
         self.setWindowTitle("Edit " + '"' + action["name"] + '"')
         self.ui.nameEdit.setText(action["name"])
         self.ui.commandEdit.setText(action["command"])
+        if action["window_title_regex"] is not None:
+            self.ui.regexEdit.setText(action["window_title_regex"])
         return self.exec_()
         
     def show_for_new_action(self):
@@ -25,7 +26,6 @@ class ActionDetailDialog(QtGui.QDialog):
         
     def accept(self):
         if ((len(str(self.ui.nameEdit.text())) > 0) and (len(str(self.ui.commandEdit.text())) > 0)):
-            return QtGui.QDialog.accept(self)
+            return QtWidgets.QDialog.accept(self)
         else:
-            return QtGui.QDialog.reject(self)
-        
+            return QtWidgets.QDialog.reject(self)
